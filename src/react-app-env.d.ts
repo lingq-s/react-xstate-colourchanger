a2f00aa50d73c82c70ec8351186d5f93b106554a
lingq-s <70804402+lingq-s@users.noreply.github.com>
2021-02-16 17:13:22 +0100

    Update react-app-env.d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,7 +6,12 @@ interface SDSContext {
     recResult: string;
     nluData: any;
     ttsAgenda: string;
-    person: string,
+    person: string;
+    day: string;
+    time: string;
+    affirm: string;
+    deny: string,
+    
 
 }
 
